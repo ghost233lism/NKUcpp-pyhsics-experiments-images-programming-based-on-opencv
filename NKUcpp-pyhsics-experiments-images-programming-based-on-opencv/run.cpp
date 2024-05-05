c638af13da9868ec14bff4fd93792c9423dfaf35
ghost233lism <jin_modi@outlook.com>
2024-05-05 18:20:06 +0800

    添加命名空间，移除无用文件

---
+++ NKUcpp-pyhsics-experiments-images-programming-based-on-opencv/run.cpp
@@ -0,0 +1,31 @@
+#include"imageClass.h"
+
+int main()
+{
+	Image img("Resources/Resources/phy.jpg");
+	img.showImage();
+	img.rotate();
+	waitKey(1);
+	std::string order;
+	while (true)
+	{
+		std::cout << "图像是否需要重新生成？[Y]/[N]" << std::endl;
+		std::cin >> order;
+		if (order == "Y")
+		{
+			double angle = img.getRotateAngle(img.getImgAfterRotate());
+			img.rotate(angle);
+			waitKey(1);
+		}
+		else if (order == "N")
+		{
+			img.eraseText();
+			break;
+		}
+		else
+		{
+			std::cout << "指令错误" << std::endl;
+		}
+	}
+	return 0;
+}
